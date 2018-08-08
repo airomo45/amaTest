1349a2856627b9074ea6f028a1aa00c87b8680c5
Romo Islam <>
2018-08-08 14:17:25 -0500

    Adding all the files

---
+++ screens/HomeScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          This is the Home Screens
+        </Text>
+
+      </View>
+    );
+  }
+
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+     alignItems: 'center' 
+  },
+
+});
